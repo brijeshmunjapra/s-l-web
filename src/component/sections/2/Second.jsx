f21e2280a5a4e921d4e55ce52c7418386135eabc
brijeshmunjapra <brijesh@solvative.com>
2025-11-17 01:54:18 +0530

    third section web view

--- src/component/sections/2/Second.jsx
+++ src/component/sections/2/Second.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Second.scss'
+import coupleImage from '../../../assets/second-section/image.webp'
 
 const Second = () => {
   return (
-    <div>Second</div>
+    <div className='second-section'>
+      <div className='second-section-content'>
+        <div className='text-container'>
+          <h1 className='main-heading'>ROMANTIC & ARTFUL</h1>
+          <h2 className='sub-heading'>WEDDING PHOTOGRAPHY</h2>
+        </div>
+        <div className='photos-container'>
+          <div className='photo-wrapper'>
+            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+          </div>
+          <div className='photo-wrapper'>
+            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+          </div>
+          <div className='photo-wrapper'>
+            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+          </div>
+          <div className='photo-wrapper'>
+            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
