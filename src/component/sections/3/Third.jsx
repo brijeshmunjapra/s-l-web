0166bf12bebc29f757afd7de8b85bf27e8026c5f
brijeshmunjapra <brijesh@solvative.com>
2025-11-17 15:49:47 +0530

    third section css changes

--- src/component/sections/3/Third.jsx
+++ src/component/sections/3/Third.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './Third.scss'
+import leftTopImage from '../../../assets/third-section/left-top.webp'
+import leftBottomImage from '../../../assets/third-section/left-bottom.webp'
+import rightImage from '../../../assets/third-section/right.webp'
 
 const Third = () => {
   return (
-    <div>Third</div>
+    <div className='third-section'>
+      <div className='third-section-container'>
+        <div className='left-images'>
+          <div className='left-top-image-wrapper'>
+            <img src={leftTopImage} alt="Couple on beach" className='left-top-image' />
+          </div>
+          <div className='left-bottom-image-wrapper'>
+            <img src={leftBottomImage} alt="Couple on swing" className='left-bottom-image' />
+          </div>
+        </div>
+        
+        <div className='center-content'>
+          <p className='book-session-text'>BOOK A SESSION NOW</p>
+          <h1 className='main-heading'>
+            Ready To Transform Everyday<br />
+            Moment Into Timeless<br />
+            Memories?
+          </h1>
+          <div className='golden-line'></div>
+          <button className='get-in-touch-btn'>GET IN TOUCH</button>
+        </div>
+        
+        <div className='right-image'>
+          <div className='right-image-wrapper'>
+            <img src={rightImage} alt="Couple with hot air balloons" className='right-image-img' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
