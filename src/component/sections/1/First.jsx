129eecd67a6521e778efd012b3d738ce23f2bb1f
brijeshmunjapra <brijesh@solvative.com>
2025-11-20 18:00:59 +0530

    first section new view

--- src/component/sections/1/First.jsx
+++ src/component/sections/1/First.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
-import './First.scss'
-import leftImage from '../../../assets/first-section/left.webp'
-import rightImage from '../../../assets/first-section/right.webp'
+import React from "react";
+import "./First.scss";
+import leftImage from "../../../assets/first-section/left.jpg";
+import rightImage from "../../../assets/first-section/right.jpg";
+import cornerSvg from "../../../assets/first-section/right-image-left-top-corner.svg";
+import topImage from "../../../assets/first-section/masterpiece.svg";
 
 const First = () => {
   return (
-    <div className='first-section'>
-      <div className='first-section-left'>
-        <img src={leftImage} alt="Portrait" className='left-image' />
+    <div className="first-section">
+      <div className="first-section-left">
+        <img src={leftImage} alt="Left" />
       </div>
-      <div className='first-section-right'>
-        <div className='headline-text'>
-          <div className='headline-line headline-line-1'>TIMELESS.</div>
-          <div className='headline-line headline-line-2'>SOULFUL.</div>
-          <div className='headline-line headline-line-3'>UNFORGETTABLE.</div>
+      <div className="first-section-top">
+        <div className="top-content">
+          <div className="top-content-text">
+            <div className="top-content-text-line-1">Where Memories</div>
+            <div className="top-content-text-line-2">Find their Modern</div>
+          </div>
+          <img src={topImage} alt="Top" />
         </div>
-        <div className='body-text'>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      </div>
+      <div className="first-section-middle">
+        <div className="middle-content">
+          <div>
+            Recognised as a leading voice in modern wedding photography and
+            filmmaking, Shade & Light has spent years shaping a visual style
+            that blends emotion, artistry, and cinematic elegance
+          </div>
+          <div>
+            Our long-standing journey has given us the privilege of creating
+            photographs and films that become treasured heirlooms crafted with
+            intention, depth, and heart.
+          </div>
+          <div>
+            {
+              "With a trusted legacy and a distinct creative vision, Shade & Light continues to reimagine how love, culture, and celebration are captured.\nWe don’t just document weddings.\nWe create art that lives on."
+            }
+          </div>
         </div>
-        <div className='right-image-container'>
-          <img src={rightImage} alt="Wedding couple" className='right-image' />
+      </div>
+      <div className="first-section-right">
+        <div className="right-image-wrapper">
+          <img src={rightImage} alt="Right" />
+          <img src={cornerSvg} alt="Corner decoration" className="corner-svg" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default First
+export default First;
