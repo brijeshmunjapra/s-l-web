809d86452f3200b743f12e00180c007d35cb2bc8
brijeshmunjapra <brijesh@solvative.com>
2025-11-25 23:40:02 +0530

    second section changes

--- src/component/sections/2/Second.jsx
+++ src/component/sections/2/Second.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Second.scss'
-import coupleImage from '../../../assets/second-section/image.webp'
+import coupleImage1 from '../../../assets/second-section/1.webp'
+import coupleImage2 from '../../../assets/second-section/2.webp'
+import coupleImage3 from '../../../assets/second-section/3.webp'
+import coupleImage4 from '../../../assets/second-section/4.webp'
 
 const Second = () => {
   return (
@@ -8,20 +11,20 @@ const Second = () => {
       <div className='second-section-content'>
         <div className='text-container'>
           <h1 className='main-heading'>ROMANTIC & ARTFUL</h1>
-          <h2 className='sub-heading'>WEDDING PHOTOGRAPHY</h2>
+          <h2 className='sub-heading'>Emotive, editorial, and deeply romantic—my wedding photography is rooted in storytelling. I document love with an artful eye and a gentle approach, capturing every fleeting glance, quiet moment, and joyful celebration, with elegance.</h2>
         </div>
         <div className='photos-container'>
           <div className='photo-wrapper'>
-            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+            <img src={coupleImage1} alt="Abhay & Shruti" className='couple-photo' />
           </div>
           <div className='photo-wrapper'>
-            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+            <img src={coupleImage2} alt="Abhay & Shruti" className='couple-photo' />
           </div>
           <div className='photo-wrapper'>
-            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+            <img src={coupleImage3} alt="Abhay & Shruti" className='couple-photo' />
           </div>
           <div className='photo-wrapper'>
-            <img src={coupleImage} alt="Abhay & Shruti" className='couple-photo' />
+            <img src={coupleImage4} alt="Abhay & Shruti" className='couple-photo' />
           </div>
         </div>
       </div>
