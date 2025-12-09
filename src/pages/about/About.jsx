15a898c71866ea3acd6a18dc1fa071f76fef6749
brijeshmunjapra <brijesh@solvative.com>
2025-12-10 02:30:57 +0530

    about us page created

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import './About.scss'
+import React from "react";
+import "./About.scss";
+import image from "../../assets/sixth-section/bg.jpg";
 
 const About = () => {
   return (
     <div className="about-page">
-     coming soon
+      <div className="about-header">
+        <div className="about-header-title">About Us</div>
+        <div className="about-header-description">
+          When an unknown printer took a galley of type and scrambled it to make
+          a type specimen book. It has survived not only five centuries, but
+          also the leap into electronic typesetting, remaining essentially
+          unchanged.
+        </div>
+      </div>
+      <div className="about-image">
+        <img src={image} alt="About Us" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
