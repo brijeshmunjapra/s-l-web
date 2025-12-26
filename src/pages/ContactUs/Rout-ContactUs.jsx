c6d3ebedef3d69e8bcf373231c43adb4d6cbfb1a
zeel-03 <5877zeelsabhaya@gmail.com>
2025-12-26 12:45:10 +0530

    Add_Video_ContectUs

--- src/pages/ContactUs/Rout-ContactUs.jsx
+++ src/pages/ContactUs/Rout-ContactUs.jsx
@@ -5,12 +5,15 @@ import "./contacUs.scss";
 const Rout_ContactUs = () => {
   return (
     <>
-    <div className="Contact_space">
-      <section>
-        
-      </section>
-        <ContactUs/>
-    </div>
+      <div className="Contact_space">
+        <div className="Contact-video">
+          <h2>
+            Start Documenting Your Story
+          </h2>
+          <video className="video" src="https://event-pdf-crm.s3.ap-south-1.amazonaws.com/wesite-images/V%26V+INSTA+STORY+2.mp4" alt="About Us" autoPlay muted loop />
+        </div>
+        <ContactUs />
+      </div>
     </>
   )
 }
